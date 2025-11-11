6ad46e6f4cd0c2e5decba61b52b5f9c194fc8620
yub4-arema <yub4.arema@gmail.com>
2025-11-11 12:03:39 +0900

    feat: Firebase設定の移動とユーティリティ関数の整理
    - Firebase設定をlibからfunctionsに移動し、FirestoreとStorageの初期化を追加
    - utils.tsを新規作成し、クラス名を結合する関数を定義
    - 不要なlib/firebase.tsとlib/utils.tsを削除
    - button.tsxとtabs.tsxでのcn関数のインポートパスを更新
    - planning.mdに機能要件の詳細を追加

---
+++ functions/types.ts
@@ -0,0 +1,37 @@
+export interface Drink {
+  id: number;
+  name: string;
+  price: number;
+  howManySold: number;
+}
+
+export interface gachaList {
+  version: number;
+  drinks: Drink[];
+  startDate: Date;
+  endDate: Date;
+  cost: number;
+}
+
+export interface Post {
+  id: number;
+  nickname: string;
+  postedAt: Date;
+  drink1_id: number;
+  drink2_id: number;
+  pictureUrl: string;
+  profits: number;
+}
+
+export interface User {
+  sumProfit: number;
+  totalGachaCount: number;
+  totalDrinkCount: number;
+  myPostsId: number[];
+}
+
+export interface AggregateData {
+  totalProfit: number;
+  totalGachaCount: number;
+  totalDrinkCount: number;
+}
